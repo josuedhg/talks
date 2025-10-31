f679aa59582885d22d975338efdb53ad0455aaf9
Josue David Hernandez Gutierrez <me@josuedhg.com>
2025-10-31 16:31:04 -0600

    Add exmaple for inheritance pattern
    Signed-off-by: Josue David Hernandez Gutierrez <me@josuedhg.com>

---
+++ linuxcabal/11-01-2025/sources/inheritance-pattern/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stddef.h>
+
+#define container_of(ptr, type, member) ({                      \
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+	(type *)( (char *)__mptr - offsetof(type,member) );})
+
+//#define container_of(ptr, type, member) (type *)( (char *)(ptr) - offsetof(type,member) )
+
+struct swimmer {
+};
+
+struct duck;
+
+void swim(struct swimmer *s)
+{
+	printf("swimming\n");
+}
+
+struct flyer {
+};
+
+void fly(struct flyer *f)
+{
+	printf("flying\n");
+}
+
+struct runner {
+};
+
+void run(struct runner *r)
+{
+	printf("running\n");
+}
+
+struct duck {
+	char sound[10];
+	struct swimmer s;
+	struct flyer f;
+	struct runner r;
+};
+
+void swim2(struct swimmer *s)
+{
+	struct duck *d = container_of(s, struct duck, s);
+	printf("swimming %s\n", d->sound);
+}
+
+int main()
+{
+	struct duck d = {"quack", };
+
+	swim((struct swimmer *)&d);
+
+	fly(&d.f);
+	run(&d.r);
+
+	swim2(&d.s);
+
+	return 0;
+}
