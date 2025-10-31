04c63e74f5cfdf6b4286aaea1196d9da1e7c06d0
Josue David Hernandez Gutierrez <me@josuedhg.com>
2025-10-30 22:12:09 -0600

    Add sample for private functions (static)
    Signed-off-by: Josue David Hernandez Gutierrez <me@josuedhg.com>

--- linuxcabal/11-01-2025/sources/factory-pattern-2/main.c
+++ linuxcabal/11-01-2025/sources/factory-pattern-2/main.c
@@ -3,10 +3,15 @@
 #include "square.h"
 
 
+//extern void square_init(struct square *s, int side);
+
 int main()
 {
 	struct square *s = square_create(5);
 	printf("Area of square is %d\n", square_area(s));
+
+//	square_init(s, 15);
+	printf("side length is %d\n", s->side);
 	square_destroy(s);
 	return 0;
 }
